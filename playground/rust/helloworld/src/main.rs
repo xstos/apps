2d8cd04c6852bb282670032e5a05d97a94597f68
Chris C <xstos@users.noreply.github.com>
2020-11-28 15:30:20 -0500

    add rust hello world

---
+++ playground/rust/helloworld/src/main.rs
@@ -0,0 +1,10 @@
+//https://www.twelve21.io/getting-started-with-rust-on-windows-and-visual-studio-code/
+
+fn main() {
+    println!("Hello World!");
+}
+
+#[test]
+fn should_fail() {
+    unimplemented!();
+}
