22dc269e99afc3ab1a748cbe1fbcc74b55aa4607
Chris C <xstos@users.noreply.github.com>
2020-12-21 22:54:29 -0500

    draw rectangle using pixelfarm rasterizer

--- playground/RasterFromScratchCSharp/RasterFromScratch/Program.cs
+++ playground/RasterFromScratchCSharp/RasterFromScratch/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using PixelFarm.CpuBlit;
+using PixelFarm.CpuBlit.PixelProcessing;
+using PixelFarm.CpuBlit.Rasterization;
+using PixelFarm.Drawing;
+using Image = System.Windows.Controls.Image;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 namespace RasterFromScratch
@@ -23,15 +27,49 @@ namespace RasterFromScratch
             image.Source = bmp;
             RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.NearestNeighbor);
             
+            MemBitmap membitmap = new MemBitmap(width, height);
+            
+            //TEST
+            //this is low-level scanline rasterizer
+            //1. create vertex store
+            VertexStore vxs = new VertexStore();
+            vxs.AddMoveTo(0, 0);
+            vxs.AddLineTo(width, 0);
+            vxs.AddLineTo(width, height);
+            vxs.AddLineTo(0, height);
+            vxs.AddCloseFigure();
+        
+            //2. create scanline rasterizer
+            ScanlineRasterizer sclineRas = new ScanlineRasterizer();
+            sclineRas.AddPath(vxs);
+
+            //3. create destination bitmap
+            DestBitmapRasterizer destBmpRasterizer = new DestBitmapRasterizer();
+
+            //4. create 32bit rgba bitmap blender
+
+            MyBitmapBlender myBitmapBlender = new MyBitmapBlender();
+            //6. attach target bitmap to bitmap blender
+            myBitmapBlender.Attach(membitmap);
+
+            //7. rasterizer sends the vector content inside sclineRas
+            //   to the bitmap blender and  
+
+            destBmpRasterizer.RenderWithColor(myBitmapBlender, //blender+ output 
+                sclineRas, //with vectors input inside
+                new ScanlinePacked8(),
+                PixelFarm.Drawing.Color.Red);
+            
             void SurfaceToScreen()
             {
+                bmp.Lock();
                 unsafe
                 {
-                    bmp.Lock();
-                    Buffer.MemoryCopy((void*) surface.Ptr, (void*) bmp.BackBuffer, surface.NumBytesInt,surface.NumBytesInt);
-                    bmp.AddDirtyRect(surface.Rect);
-                    bmp.Unlock();
+                    IntPtr mem_ptr = membitmap.GetRawBufferHead();
+                    Buffer.MemoryCopy((byte*)mem_ptr, (byte*) bmp.BackBuffer, surface.NumBytesInt,surface.NumBytesInt);
                 }
+                bmp.AddDirtyRect(surface.Rect);
+                bmp.Unlock();
             }
             
             var win = new Window
@@ -48,6 +86,16 @@ namespace RasterFromScratch
             win.Content = image;
             var app = new Application();
             app.Run(win);
+        }
+    }
+    class MyBitmapBlender : BitmapBlenderBase
+    {
+
+        //custom implementation of BitmapBlender
+
+        public override void WriteBuffer(int[] newbuffer)
+        {
+
         }
     }
 }
