51673b5dcdf4ee569999d693bf988ad5be581993
Chris C <xstos@users.noreply.github.com>
2021-01-08 12:02:38 -0500

    Add empty Core WPF app

---
+++ playground/CoreWpfExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+namespace CoreWpfExample
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var win = new Window
+            {
+                Left = 0,
+                Top = 0,
+                Width = 1200,
+                Height = 1200,
+            };
+            win.Content = new Button() {Content = "hi!"};
+            var app = new Application();
+            app.Run(win);
+        }
+
+    }
+}
