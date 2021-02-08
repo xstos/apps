c159ec675ba5561850de82907f5bfe21e623eac4
Chris C <xstos@users.noreply.github.com>
2021-02-07 20:44:12 -0500

    nreact demo

--- KriterisEngine/ReactRedux/ExampleApp.cs
+++ KriterisEngine/ReactRedux/ExampleApp.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-
+using NReact;
+using static KriterisEngine.NExt;
+using static KriterisEngine.Common;
 namespace KriterisEngine.ReactRedux
 {
     public class ExampleApp
@@ -14,28 +18,83 @@ namespace KriterisEngine.ReactRedux
             Store.New().Out(out var store);
             store.Dispatch.Out(out var dispatch);
             store.Subscribe.Out(out var subscribe);
+
+            var items = new ObservableCollection<string>();
+            int i = 0;
             UIElement App()
             {
                 MakeRootPanel().Out(out var mainPanel);
+                mainPanel.Add(AutoComplete.ExampleUsage());
+
+                var mirror = new List<Action<string>>();
+                void InitCell(El instance)
+                {
+                    var (_, setText) = instance.Prop(Props.Text, "Sup");
+                    mirror.Add(setText);
+                };
+                Enumerable
+                    .Range(0, 10)
+                    .Select(i => El.New(RenderCell, InitCell))
+                    .ToArray()
+                    .Out(out var cells);
+                El.New(RenderPanel)
+                    .Out(out var nPanel);
+
+                NElement RenderCell(El instance)
+                {
+                    var (getText, _) = instance.Prop(Props.Text, "Sup");
+                    
+                    return N<TextBox>()
+                        .Text(getText())
+                        .Margin(3)
+                        .TextChanged(sender =>
+                        {
+                            var text = sender.As<TextBox>().Text;
+                            mirror.ForEach(setter => setter(text));
+                        });
+                }
+                NElement RenderPanel(El instance)
+                {
+                    return N<StackPanel>()
+                        .Children(cells)
+                        .Background(Brushes.DarkSalmon);
+                }
                 
+                New<ContentControl>().Out(out var content);
+                mainPanel.Add(content);
+                content.Render(nPanel);
+
                 WrapPanel MakeRootPanel()
                 {
-                    new WrapPanel().Out(out var panel).MakePanelFocusable();
-                    new Button().Out(out var addCounterButton);
+                    New<WrapPanel>().Out(out var panel).MakePanelFocusable();
+                    New<Button>().Out(out var addCounterButton);
                     addCounterButton.Content = "Add Counter";
                     panel.Children.Add(addCounterButton);
                     
                     addCounterButton.Click += (sender, args) =>
                     {
                         dispatch(("new.button", Id.New()));
+                        panel.Children.Add(MakeTextBox(Id.New()));
+                        items.Add(i++.ToString());
                     };
                     
                     return panel;
                 }
 
+                
+                UIElement MakeTextBox(Id id)
+                {
+                    
+                    New<DockPanel>().Out(out var container);
+                    New<TextBox>().Out(out var name);
+                    New<ListBox>().Out(out var lb);
+                    name.DataContext = items;
+
+                    return name;
+                }
                 Button MakeCounterButton(Id id)
                 {
-                    new Button().Out(out var button);
+                    New<Button>().Out(out var button);
                     button.Content = 0;
                     button.PreviewMouseDown += (sender, args) =>
                     {
@@ -80,7 +139,7 @@ namespace KriterisEngine.ReactRedux
                 
                 return mainPanel;
             }
-
+            
             window.Closed += (sender, args) =>
             {
                 store.CloseDb();
