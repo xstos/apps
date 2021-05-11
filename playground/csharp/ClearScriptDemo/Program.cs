960f40fff5061e6a99a5fe491a76d7c75418031d
Chris C <xstos@users.noreply.github.com>
2021-05-11 19:26:32 -0400

    ClearScriptDemo

---
+++ playground/csharp/ClearScriptDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.ClearScript.V8;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+namespace ClearScriptDemo
+{
+    public class Program
+    {
+        public static Action<string> LogMethod;
+
+        public static void Log(string s)
+        {
+            LogMethod(s);
+        }
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            
+            var win = new Window
+            {
+                Left = 0,
+                Top = 0,
+                Width = 400,
+                Height = 300,
+            };
+            var txt = new TextBox();
+            win.Content = txt;
+            var app = new Application();
+            LogMethod = (s) => txt.AppendText(s);
+
+            var engine = new V8ScriptEngine();
+            engine.AddHostType("Prog", typeof(Program));
+
+            engine.Execute(
+            @"
+            var x = { foo: 'hi'}; 
+            var { foo } = x;
+            Prog.Log(foo); 
+            ");
+
+            app.Run(win);
+        }
+
+    }
+}
