06e83fa894cfa4082492e6cb46263a91876821ef
Chris C <xstos@users.noreply.github.com>
2021-05-22 10:05:16 -0400

    solid js demo

---
+++ playground/js/solid-js-demo/src/App.js
@@ -0,0 +1,25 @@
+// noinspection JSXNamespaceValidation
+
+import './App.css';
+
+function App() {
+  return (
+    <div class="App">
+      <header class="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          class="App-link"
+          href="https://github.com/ryansolid/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
