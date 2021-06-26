40339a1eb1acf176460fba4547d1619740c0456d
Chris C <xstos@users.noreply.github.com>
2021-06-26 18:23:43 -0400

    source generator get path example

---
+++ playground/csharp/IndexerSourceGenerator/GeneratorUsageExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b32fbede-85a7-48a9-80b2-75f865790d3b")]
---
+++ playground/csharp/IndexerSourceGenerator/IndexerSourceGenerator/Indexer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+
+namespace IndexerSourceGenerator
+{
+    
+    public class Watcher : ISyntaxReceiver
+    {
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            
+        }
+    }
+    [Generator]
+    public class Indexer: ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(()=>new Watcher());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+#if DEBUG
+            if (!Debugger.IsAttached)
+            {
+                Debugger.Launch();
+            }
+#endif
+            var file = context.Compilation.SyntaxTrees.First(st =>
+                Path.GetFileName(st.FilePath).ToLower().Contains("assemblyinfo.cs"));
+        }
+
+    }
+
+    
+}
