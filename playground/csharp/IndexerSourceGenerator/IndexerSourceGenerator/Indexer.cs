97586c84fc77954c2c7f5f191f335181a4037aaf
Chris C <xstos@users.noreply.github.com>
2021-06-26 20:36:34 -0400

    write index while compiling

--- playground/csharp/IndexerSourceGenerator/IndexerSourceGenerator/Indexer.cs
+++ playground/csharp/IndexerSourceGenerator/IndexerSourceGenerator/Indexer.cs
@@ -7,26 +7,26 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace IndexerSourceGenerator
 {
     
     public class Watcher : ISyntaxReceiver
     {
+        public Action<SyntaxNode> Action { get; set; }
         public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
         {
-            
+            Action(syntaxNode);
         }
     }
     [Generator]
     public class Indexer: ISourceGenerator
     {
+        private string artifacts = null;
+        private string index = null;
+        private List<string> names = new List<string>();
         public void Initialize(GeneratorInitializationContext context)
-        {
-            context.RegisterForSyntaxNotifications(()=>new Watcher());
-        }
-
-        public void Execute(GeneratorExecutionContext context)
         {
 #if DEBUG
             if (!Debugger.IsAttached)
@@ -34,11 +34,46 @@ namespace IndexerSourceGenerator
                 Debugger.Launch();
             }
 #endif
-            var file = context.Compilation.SyntaxTrees.First(st =>
-                Path.GetFileName(st.FilePath).ToLower().Contains("assemblyinfo.cs"));
+            context.RegisterForSyntaxNotifications(()=>new Watcher()
+            {
+                Action = node =>
+                {
+                    if (node is ClassDeclarationSyntax syn)
+                    {
+                        names.Add(syn.Identifier.ValueText);
+                    }
+                }
+            });
         }
 
-    }
+        public void Execute(GeneratorExecutionContext context)
+        {
 
-    
+            if (artifacts == null)
+            {
+                IEnumerable<FileInfo> GetSourcePaths() => 
+                    context.Compilation.SyntaxTrees.Select(st => new FileInfo(st.FilePath));
+
+                var dir = GetSourcePaths().First(fi =>
+                    fi.Name.Equals("assemblyinfo.cs", StringComparison.InvariantCultureIgnoreCase))
+                    .Directory;
+
+                while (!dir.GetDirectories(".git").Any()) //find git root
+                {
+                    dir = dir.Parent;
+                }
+
+                artifacts = Path.Combine(dir.FullName, "artifacts");
+                if (!Directory.Exists(artifacts))
+                {
+                    Directory.CreateDirectory(artifacts);
+                }
+
+                index = Path.Combine(artifacts, context.Compilation.Assembly.Name + ".iocindex.json");
+                File.Delete(index);
+            }
+
+            File.AppendAllLines(index, names);
+        }
+    }
 }
