217096b959809f98bb539ec8fcbc0bea4ababd1d
Chris C <xstos@users.noreply.github.com>
2021-06-30 12:17:52 -0400

    easy contexts in react

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # apps
 
-KriterisEdit (currently in development) will be a bulk XML editing tool.
+Numerous experiments
---
+++ playground/js/KriterisEngine/src/experimental/app2/easyContextExample.tsx
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react'
+import ReactDOM from 'react-dom'
+
+function BuildContext(props) {
+  const { value, defaultValue, children, useInit } = props
+
+  const MyContext = React.createContext(defaultValue)
+  const useMyContext = () => useContext(MyContext)
+  useInit(useMyContext)
+  return <MyContext.Provider value={value}>{children}</MyContext.Provider>
+}
+
+function useInit() {
+  let cached = null
+  return function (value) {
+    if (cached) return cached()
+    cached = value
+  }
+}
+
+const useHello = useInit()
+
+function InnerComponent(props) {
+  const hello = useHello()
+  return <div>{hello}</div>
+}
+
+function App() {
+  return (
+    <div>
+      <BuildContext value="hello!" defaultValue="default!!" useInit={useHello}>
+        <InnerComponent />
+      </BuildContext>
+      <InnerComponent />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+/*
+prints
+hello!
+default!!
+* */
