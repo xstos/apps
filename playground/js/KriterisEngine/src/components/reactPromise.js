1bd75a3269d7f38c5b14eaf04d53856daea71345
Chris C <xstos@users.noreply.github.com>
2021-07-07 14:10:48 -0400

    upgrade

--- playground/js/KriterisEngine/src/components/reactPromise.js
+++ playground/js/KriterisEngine/src/components/reactPromise.js
@@ -1,34 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function PromiseHandler(props) {
-  const {promise, children, thenOrCatch, promisePassed, ...rest} = props
-  const [result, setResult] = React.useState(null)
-  promise[thenOrCatch](setResult)
-  return result ? cloneReactChildren(children, { promiseResult: { result, passed: promisePassed }, ...rest }) : null
+export const nameof = obj => Object.keys(obj)[0]
+const injectProps = props => cloneReactChildren(props.children, {promiseResult: props.promiseResult})
+export const ResolveR = props => injectProps(props)
+export const RejectR = props => injectProps(props)
+export const PendingR = props => injectProps(props)
+
+export function PromiseR(props) {
+  const { getPromise, children, ...rest } = props
+  const [resolve, setResolve] = React.useState(null)
+  const [reject, setReject] = React.useState(null)
+  useEffect(()=>{
+    getPromise().then(setResolve).catch(setReject)
+  },[])
+
+  const isOurs = (child) => ({ ResolveR, RejectR, PendingR })[child.type.name]
+  function clone(child, type, success, value) {
+    if (!React.isValidElement(child) || !isOurs(child)) return child
+    if (type !== child.type.name) return null
+    const inject = {promiseResult: {success, value}}
+    return React.cloneElement(child, {...child.props, ...inject})
+  }
+
+  const map = (keyObj, success, value) =>
+    React.Children.map(children, (child) => clone(child, nameof(keyObj), success, value))
+
+  if (resolve) return map({ResolveR}, true, resolve)
+  if (reject) return map({RejectR}, true, reject)
+  return map({PendingR}, false, null)
 }
 
-const Pass = props => <PromiseHandler thenOrCatch={'then'} promisePassed={true} {...props} />
-const Fail = props => <PromiseHandler thenOrCatch={'catch'} promisePassed={false} {...props} />
-
-function Wait(props) {
-  const [done, setDone] = React.useState(false)
-  props.promise.finally(() => setDone(true))
-  if (done) return null
-  return <>{props.children}</>
-}
-
-//handles promises as react components: Pass, Fail, Wait. See usages below.
-export function ReactPromise(getPromise, callback) {
-  const promise = getPromise()
-  const ret = {
-    Pass: (props) => <Pass promise={promise} {...props}/>,
-    Fail: (props) => <Fail promise={promise} {...props}/>,
-    Wait: (props) => <Wait promise={promise} {...props}/>,
-  }
-  if (callback) {
-    return callback(ret)
-  }
-  return ret
+// https://stackoverflow.com/a/32371612/1618433
+export function cloneReactChildren(children, props) {
+  const clone = child => React.isValidElement(child) ? React.cloneElement(child,{ ...child.props, ...props }) : child
+  return React.Children.map(children, clone)
 }
 
 // <Examples>
@@ -36,41 +41,20 @@ const makeExamplePromise = () => new Promise((resolve, reject) => setTimeout(()=
 const PromiseResultViewer = ({ promiseResult }) => JSON.stringify(promiseResult)
 
 export function ExampleComponentUsage(props) {
-  return ReactPromise(makeExamplePromise, ({Pass, Fail, Wait}) => (
-    <div>
-    <Pass>
-      <PromiseResultViewer/>
-    </Pass>
-    <Fail>
-      <PromiseResultViewer/>
-    </Fail>
-    <Wait>
-      waiting for promise to resolve or reject
-    </Wait>
-    </div>
-  ))
-}
-
-export function ExampleComponentAlternateUsage(props) {
-  const {Pass, Fail, Wait} = ReactPromise(makeExamplePromise)
   return (
-    <div>
-    <Pass>
-      <PromiseResultViewer/>
-    </Pass>
-    <Fail>
-      <PromiseResultViewer/>
-    </Fail>
-    <Wait>
-      waiting for promise to resolve or reject
-    </Wait>
-  </div>
+    <PromiseR getPromise={makeExamplePromise}>
+      <ResolveR>
+        resolve
+        <PromiseResultViewer></PromiseResultViewer>
+      </ResolveR>
+      <RejectR>
+        reject
+        <PromiseResultViewer></PromiseResultViewer>
+      </RejectR>
+      <PendingR>
+        pending
+        <PromiseResultViewer></PromiseResultViewer>
+      </PendingR>
+    </PromiseR>
   )
-}
-// </Examples>
-
-// https://stackoverflow.com/a/32371612/1618433
-export function cloneReactChildren(children, props) {
-  const clone = child => React.isValidElement(child) ? React.cloneElement(child,{ ...child.props, ...props }) : child
-  return React.Children.map(children, clone)
 }
