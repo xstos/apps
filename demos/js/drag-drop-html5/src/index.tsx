4cb582c71717dddba6e2361ce08c5f6254aaa7d8
Chris C <xstos@users.noreply.github.com>
2022-06-10 00:25:15 -0400

    implement drag drop demo with hyperactiv

--- demos/js/drag-drop-html5/src/index.tsx
+++ demos/js/drag-drop-html5/src/index.tsx
@@ -1,12 +1,72 @@
 import './index.css';
 import {store as createStore, watch} from 'hyperactiv/src/react'
 import * as ReactDOM from "react-dom";
+import {DndProvider, useDrag, useDrop} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
-const state = createStore([[{id:0}],[]])
-const Render = watch(()=>{
-  return state.map(arr=>{
-    return arr.map(c=><div>{JSON.stringify(c)}</div> )
-  })
+const state = createStore({
+  items: ['a','b','c','d'],
 })
+const Render = watch(() =>
+  state.items.map((item, i) => <DndDiv id={i}>{item}</DndDiv>))
 
-ReactDOM.render(<Render/>,document.getElementById('root'))
+function Provider(props) {
+  return <DndProvider backend={HTML5Backend}>{props.children}</DndProvider>
+}
+
+function swapIndexes(arr, dropId, draggedId) {
+  const old = arr[dropId]
+  arr[dropId] = arr[draggedId]
+  arr[draggedId] = old
+}
+
+function DndDiv(props) {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: '1',
+    item: {id: props.id},
+    collect: (monitor) => {
+      return ({
+        isDragging: !!monitor.isDragging(),
+      });
+    }
+  }))
+
+  const [{ isOver, canDrop }, drop] = useDrop(
+    () => ({
+      accept: '1',
+      drop: (item, monitor) => {
+        let destId = props.id
+        let srcId = item.id
+        swapIndexes(state.items, destId, srcId);
+      },
+      canDrop: (item, monitor) => {
+        return item.id !== props.id
+      },
+      collect: (monitor) => {
+        return ({
+          isOver: !!monitor.isOver(),
+          canDrop: !!monitor.canDrop(),
+        })
+      }
+    }),
+    []
+  )
+  return <button
+    ref={(el) => drag(drop(el))}
+    style={{
+      opacity: isDragging ? 0.5 : 1,
+      cursor: 'move',
+      minWidth: '6ch',
+      minHeight: '6ch',
+      border: '1px solid red',
+      display: 'inline-block'
+    }}
+  >
+    {props.children}
+  </button>
+}
+const App = <Provider>
+  <Render/>
+</Provider>
+
+ReactDOM.render(App,document.getElementById('root'))
