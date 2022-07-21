af4827bf8cb28410c17cb6bec376925704d76b37
Chris C <xstos@users.noreply.github.com>
2022-07-21 14:12:44 -0400

    heading toward homogeneity w/ jsx and reg js

---
+++ playground/js/kreact-proxy/src/index.tsx
@@ -0,0 +1,82 @@
+import './index.css';
+import React from "react";
+import ReactDOM from 'react-dom'
+import {bindkeys} from "./io";
+
+declare var v: any
+declare var o: any
+declare var global: any
+
+export const cursorBlock = '█'
+
+let seed=0
+
+//https://stackoverflow.com/questions/67759433/typescript-parcel-new-jsx-transform-react-is-not-defined-error
+//https://stackoverflow.com/a/68238924/1618433
+//https://dev.to/gugadev/use-custom-elements-in-react-using-a-custom-jsx-pragma-3kc
+let reactMode = false
+export function jsx(tag: any, props: Record<string, any>, ...children: any[]) {
+  if (reactMode) return React.createElement(tag,props,...children)
+  //console.log({type,props,children})
+  if (props) {
+    return {type: tag, props, children}
+  }
+  //console.log(arguments)
+  return {tag, children}
+}
+
+function newProxy(state, handler) {
+  return new Proxy(state, handler)
+}
+
+export const intellisense = <menu>
+  <item selected>foo</item>
+  <item>bar</item>
+</menu>
+
+const app = <cells>
+  <pi>{3.14159}</pi>
+  <radius>{3}</radius>
+  <exponent>{2}</exponent>
+  <area><pow><radius/><exponent/></pow></area>
+</cells>
+
+function makeProxy(type) {
+  let handler = null
+  handler = {
+    get(target, key) {
+      if (key.startsWith('_')) {
+        return target[key]
+      }
+      target._data.push(['get', key])
+      console.log(target._data)
+      return newProxy(target, handler)
+    },
+    apply(target, thisArg, args) {
+      target._data.push(['apply',...args.map(a=>a._data ? a._data : a)])
+      console.log(target._data)
+      return newProxy(target, handler)
+    },
+  }
+  const f = ()=>{}
+  f._data = [type]
+  return newProxy(f, handler)
+}
+function defProp(key,type) {
+  Object.defineProperty(global, key, {
+    get: () => makeProxy(type)
+  })
+}
+
+defProp('v','var')
+defProp('o','op')
+
+
+v.a1(123)
+v.b1(o.equals(v.a1))
+
+reactMode=true
+ReactDOM.render(
+  <div>hello</div>,
+  document.getElementById('root')
+)
---
+++ playground/js/kreact-proxy/src/io.ts
@@ -0,0 +1,34 @@
+import keyboard from 'keyboardjs'
+import {equalsAny} from "./util";
+
+export function bindkeys(onkey) {
+  const lettersArray = Array.from(
+    '`abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}\\;:\'"<>,./?'
+  )
+  //keyboard.bind([...lettersArray, 'space', 'enter', 'escape', 'backspace', 'left', 'right', 'up', 'down', 'delete'], pressed)
+  // keyboard.bind('ctrl + enter', ()=>{
+  //   debugger
+  // })
+  document.addEventListener('keydown', event => {
+    const key = event.key.toLowerCase()
+
+    if (key==="control" || key==="alt" || key==="shift") return
+    const mod = [event.ctrlKey && "ctrl", event.altKey && "alt", event.shiftKey && "shift"]
+      .filter(v=>v)
+
+    const foo = [...mod, key].join("+")
+    if (equalsAny(foo, 'ctrl+a', 'tab')) {
+      event.preventDefault()
+    }
+    pressed({ tag: 'io', key: foo })
+
+
+  });
+  //keyboard.bind('ctrl',(e)=>console.log(e))
+  function pressed(e) {
+    console.log(e)
+    const o = { tag: "io",  key: e.key.toLowerCase() }
+    onkey(o)
+  }
+}
+
