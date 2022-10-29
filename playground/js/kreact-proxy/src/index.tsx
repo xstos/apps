971380a700e1ca9d3aa839a8cdbf49f92a5faac6
Chris C <xstos@users.noreply.github.com>
2022-10-28 22:08:30 -0400

    state machine abstraction!

--- playground/js/kreact-proxy/src/index.tsx
+++ playground/js/kreact-proxy/src/index.tsx
@@ -4,6 +4,8 @@ import hyperactiv from 'hyperactiv'
 import ReactDOM from "react-dom"
 import {bindkeys} from "./io"
 import {customJsx} from "./reactUtil"
+import {cellx} from "cellx"
+import  * as jsondiffpatch from 'jsondiffpatch'
 //import {BoxComponent, DragDropDemo} from "./dragdrop"
 const {observe, computed} = hyperactiv
 let jsxCallback = customJsx
@@ -37,6 +39,7 @@ function render(jsx) {
   const ret = document.createElement(jsx.type)
   if (jsx.type==='span') {
     ret.tabIndex=0
+    ret.classList.add('drg')
   }
   if (jsx.children) {
     const children = jsx.children.map(c=>{
@@ -51,20 +54,129 @@ function render(jsx) {
 
   return ret
 }
-function hookupEventHandlers() {
-  let pointerDown = false
-  let dragging = false
-  let startX,startY
-  let selected = []
-  let selectedBounds = []
-  function emptyHandler(e) {
-    return false
+
+function cx(o) {
+  const cell = cellx({
+    current: o,
+   })
+  function accessor(arg, data) {
+    if (arguments.length===0) {
+      return cell()
+    }
+    const obj = cell()
+    const {current} = obj
+    if (arg===current) return obj
+    const ret = {
+      last: current,
+      current: arg,
+      data
+    }
+    cellx(ret)
+    return ret
   }
+  return accessor
+}
+type Pred<T> = (value: T) => boolean
+type Callback<T> = (value: T) => any
+function transitioned(o,a,b) {
+  return o && o[0] === a && o[1] === b
+}
+function hookupEventHandlersFRP() {
+  const initialStateSansDiff = {
+    mouseState: 'unknown',
+    startX: 0,
+    startY: 0,
+    x: 0,
+    y: 0,
+    dragging: false,
+    el: null
+  }
+  const actions = {
+    onClick(s: TState) {
+      toggleClass(s.el,'sel')
+    },
+    dragStart(s: TState) {
+
+    }
+  }
+  const initialState = {
+    ...initialStateSansDiff,
+    diff: jsondiffpatch.diff({},initialStateSansDiff)
+  }
+  type TState = typeof initialState
+  const history: TState[] = [initialState]
+  const stateCell = cellx(0)
+  function state(o?: Partial<TState>): TState {
+    const prev = history[stateCell()]
+    if (typeof o === 'undefined') {
+      return prev
+    }
+    const l = history.length
+    const ret = { ...prev, ...o }
+    const diff = jsondiffpatch.diff(prev,ret)
+    if (diff === undefined) {
+      return prev
+    }
+    ret.diff = diff
+    history.push(ret)
+    console.log(JSON.stringify(ret), new Date() + '')
+    stateCell(l)
+    return ret
+  }
+  function isClick(s:TState): boolean {
+    const delta = 6
+    const diffX = Math.abs(s.x - s.startX)
+    const diffY = Math.abs(s.y - s.startY)
+    return diffX < delta && diffY < delta
+  }
+  const setDrag = cellx(()=>{
+    const s = state()
+    if (s.mouseState !== 'down' || isClick(s)) return
+    const ret = state({ dragging: true})
+    actions.dragStart(s)
+    return ret
+  }).onChange(()=> {})
+
+  const whileDragging = cellx(() => {
+    const s = state()
+    if (!s.dragging) return
+
+  })
+  const wasClicked = cellx(() => {
+    const s = state()
+    if (s.dragging) return
+    if (!transitioned(s.diff.mouseState,'down','up')) return
+    actions.onClick(s)
+  }).onChange(()=> {})
+  const dragDone = cellx(() => {
+    const s = state()
+    if (!s.dragging) return
+    if (transitioned(s.diff.mouseState,'down', 'up')) {
+      state({dragging: false})
+    }
+  }).onChange(()=> {})
+
+  document.addEventListener('pointerdown', (e)=> {
+    state({ mouseState: 'down', startX: e.clientX, startY: e.clientY})
+  })
+  document.addEventListener('pointermove',(e)=>{
+    state({x: e.clientX, y: e.clientY})
+  })
+  document.addEventListener('pointerup',(e)=>{
+    let el = e.path[0]
+    el = el === rootEL ? document.createElement('div') : el
+    state({mouseState: 'up', el})
+  })
+}
+function hookupEventHandlers() {
+  const emptyHandler = e => false
   let isClick = emptyHandler
   let handleMove = emptyHandler
   let dragItemsToCursor = emptyHandler
   let resetDraggedItems = emptyHandler
   document.addEventListener('pointerdown', (clickEvent)=>{
+    const targ = clickEvent.path[0]
+    if (!targ.classList.contains('drg')) return
     isClick = (e) => {
       const delta = 6
       const diffX = Math.abs(e.clientX - clickEvent.clientX);
@@ -78,8 +190,8 @@ function hookupEventHandlers() {
         const selected = Array.from(document.querySelectorAll('.sel'))
         const oldStyles = selected.map(n=>({p: n.style.position, t: n.style.transform}))
         const selectedBounds = selected.map(s=>s.getBoundingClientRect())
-        dragItemsToCursor = (ee) => {
-          const [deltaX,deltaY] = [ee.clientX - clickEvent.clientX, ee.clientY - clickEvent.clientY]
+        dragItemsToCursor = (dragEvent) => {
+          const [deltaX,deltaY] = [dragEvent.clientX - clickEvent.clientX, dragEvent.clientY - clickEvent.clientY]
           selected.forEach((n,i)=>{
             n.style.position='absolute'
             n.style.transform = "translate3d("+deltaX+"px,"+deltaY+"px, 0px)";
@@ -120,4 +232,4 @@ function toggleClass(el,cls) {
     el.classList.add(cls)
   }
 }
-hookupEventHandlers()
+hookupEventHandlersFRP()
