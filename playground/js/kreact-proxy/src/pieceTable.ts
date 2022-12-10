805638005cfe704386323297012861911ef6fc96
Chris C <xstos@users.noreply.github.com>
2022-12-10 13:20:39 -0500

    piece table with history

--- playground/js/kreact-proxy/src/pieceTable.ts
+++ playground/js/kreact-proxy/src/pieceTable.ts
@@ -2,8 +2,13 @@
  * A piece table is an efficient data structure to track changes to text.
  * See <https://www.cs.unm.edu/~crowley/papers/sds/node15.html> for details
  * https://github.com/sparkeditor/piece-table/blob/master/index.js
+ * https://darrenburns.net/posts/piece-table/
  */
-export function PieceTable2<T>(fileText: T[]) {
+import Immutable from "immutable"
+
+let log = console.log
+const outOfBoundsError = new Error("Index out of bounds");
+export function PieceTable<T>(fileText: T[]) {
   /**
    * The file buffer represents the original file text
    * @private
@@ -17,41 +22,16 @@ export function PieceTable2<T>(fileText: T[]) {
    * @type {string}
    */
   let add: T[] = [];
+  const firstPiece = {
+    addBuffer: false, //true if the Piece points to the add buffer, false if it points to the file buffer
+    offset: 0, //the index in the buffer where the Piece starts
+    length: file.length //the length of the Piece
+  }
+  let pieceTable = [firstPiece];
 
-  /**
-   * A Piece describes a window into the file or the add buffer
-   * @typedef {Object} Piece
-   * @property {boolean} addBuffer - true if the Piece points to the add buffer, false if it points to the file buffer
-   * @property {number} offset - the index in the buffer where the Piece starts
-   * @property {number} length - the length of the Piece
-   * @private
-   */
-
-  /**
-   * The piece table describes the sequence as a series of Pieces
-   * @type {Piece[]}
-   * @private
-   */
-  const pieceTable = [{
-    addBuffer: false,
-    offset: 0,
-    length: file.length
-  }];
-
-  /**
-   * An error thrown when an attempt is made to access the sequence at an invalid index
-   * @private
-   * @type {Error}
-   */
-  const outOfBoundsError = new Error("Index out of bounds");
-
-  /**
-   * Returns the index in the piece table of the piece that contains the character at offset, and the offset into that piece's buffer corresponding to the offset
-   * @private
-   * @param offset - an index into the sequence (not into the piece table)
-   * @returns {number[]} A 2-number array where the first number is the piece table index, and the second is the offset into that piece's buffer
-   */
-  function sequenceOffsetToPieceIndexAndBufferOffset(offset: number) {
+  let history = Immutable.fromJS([[firstPiece]])
+   //Returns the index in the piece table of the piece that contains the character at offset, and the offset into that piece's buffer corresponding to the offset
+  function sequenceOffsetToPieceIndexAndBufferOffset(offset: number): [number,number] {
     if (offset < 0) {
       throw outOfBoundsError;
     }
@@ -67,35 +47,64 @@ export function PieceTable2<T>(fileText: T[]) {
     throw outOfBoundsError;
   };
 
-  /**
-   * Inserts a string into the piece table
-   * @param {string} str - the string to insert
-   * @param {number} offset - the offset at which to insert the string
-   */
-  this.insert = function(str: T[], offset: number) {
+  function pushHistory() {
+    let prev = history.last()
+    pieceTable.forEach((piece,i)=>{
+      let p = prev.get(i)
+      if (!p) {
+        prev = prev.set(i,Immutable.Map(piece))
+      } else {
+        prev = prev.set(i,p.merge(piece))
+      }
+    })
+
+    history=history.push(prev)
+    const h =getHistory(history.size-1)
+    log(h)
+  }
+  function getHistoryLastIndex() {
+    return history.size-1
+  }
+  function getHistory(index: number): T[] {
+    const old = pieceTable
+    pieceTable = history.get(index).map(h=>h.toJS())
+    const doc = getSequence()
+    pieceTable=old
+    return doc
+  }
+  function getHistories() {
+    return history.map((h,i)=>getHistory(i))
+  }
+  function insert(str: T[], offset: number) {
     if (str.length === 0) {
       return;
     }
+
     const addBufferOffset = add.length;
     add = add.concat(str)
+
+    const newPiece = {
+      addBuffer: true,
+      offset: addBufferOffset,
+      length: str.length
+    }
+
     const [pieceIndex, bufferOffset] = sequenceOffsetToPieceIndexAndBufferOffset(offset);
     let originalPiece = pieceTable[pieceIndex];
     // If the piece points to the end of the add buffer, and we are inserting at its end, simply increase its length
     if (originalPiece.addBuffer && bufferOffset === originalPiece.offset + originalPiece.length && originalPiece.offset + originalPiece.length === addBufferOffset) {
       originalPiece.length += str.length;
+      pushHistory()
       return;
     }
+
     const insertPieces = [
       {
         addBuffer: originalPiece.addBuffer,
         offset: originalPiece.offset,
         length: bufferOffset - originalPiece.offset
       },
-      {
-        addBuffer: true,
-        offset: addBufferOffset,
-        length: str.length
-      },
+      newPiece,
       {
         addBuffer: originalPiece.addBuffer,
         offset: bufferOffset,
@@ -104,19 +113,17 @@ export function PieceTable2<T>(fileText: T[]) {
       return piece.length > 0;
     });
     pieceTable.splice(pieceIndex, 1, ...insertPieces);
-  };
 
-  /**
-   * Deletes a string from the piece table
-   * @param {number} offset - the offset at which to begin deletion
-   * @param {number} length - the number of characters to delete. If negative, deletes backwards
-   */
-  this.delete = function(offset: number, length: number) {
+    pushHistory()
+    //log(pieceTable,'ins',str,offset)
+  };
+  function remove(offset: number, length: number) {
     if (length === 0) {
       return;
     }
     if (length < 0) {
-      return this.delete(offset + length, -length);
+      remove(offset + length, -length);
+      return
     }
     if (offset < 0) {
       throw outOfBoundsError;
@@ -133,11 +140,13 @@ export function PieceTable2<T>(fileText: T[]) {
       if (initialBufferOffset === piece.offset) {
         piece.offset += length;
         piece.length -= length;
+        pushHistory()
         return;
       }
       // Or at the end of the piece?
       else if (finalBufferOffset === piece.offset + piece.length) {
         piece.length -= length;
+        pushHistory()
         return;
       }
     }
@@ -157,35 +166,18 @@ export function PieceTable2<T>(fileText: T[]) {
     });
 
     pieceTable.splice(initialAffectedPieceIndex, finalAffectedPieceIndex - initialAffectedPieceIndex + 1, ...deletePieces);
+    pushHistory()
+    //log(pieceTable,'del',offset, length)
   };
-
-  /**
-   * Gets the sequence as a string
-   * @returns {string} The sequence
-   */
-  this.getSequence = function() {
+  function getSequence() {
     let str: T[][] = [];
     pieceTable.forEach(function(piece) {
-      if (piece.addBuffer) {
-        str.push(slice(add,piece.offset, piece.length))
-        //str += add.substr(piece.offset, piece.length);
-      }
-      else {
-        str.push(slice(file,piece.offset, piece.length))
-        //str += file.substr(piece.offset, piece.length);
-      }
+      let chunk = piece.addBuffer ? add : file
+      str.push(slice(chunk,piece.offset, piece.length))
     });
     return Array.prototype.concat(...str);
   };
- function slice(arr: T[],offset:number,len:number): T[] {
-   const ret = arr.slice(offset,offset+len)
-   return ret
- }
-  /**
-   * PieceTable iterator
-   * @private
-   */
-  this[Symbol.iterator] = function() {
+  function iter() {
     let currentPiece = 0;
     let currentOffset = 0;
     return {
@@ -214,15 +206,9 @@ export function PieceTable2<T>(fileText: T[]) {
       }
     };
   };
-
-  /**
-   * Gets a string of a particular length from the piece table at a particular offset
-   * @param {number} offset - the offset from which to get the string
-   * @param {number} length - the number of characters to return from the offset. If negative, looks backwards
-   */
-  this.stringAt = function(offset: number, length: number) {
+  function stringAt(offset: number, length: number): T[] {
     if (length < 0) {
-      return this.stringAt(offset + length, -length);
+      return stringAt(offset + length, -length);
     }
     let str: T[][] = [];
     const [initialPieceIndex, initialBufferOffset] = sequenceOffsetToPieceIndexAndBufferOffset(offset);
@@ -253,26 +239,36 @@ export function PieceTable2<T>(fileText: T[]) {
         }
       }
     }
-    return str.length === 0 ? undefined : Array.prototype.concat(...str)
+    return str.length === 0 ? [] : Array.prototype.concat(...str)
     //return str === "" ? undefined : str;
   };
+  return {
+    insert,
+    remove,
+    getSequence,
+    stringAt,
+    getHistories,
+    [Symbol.iterator]: iter,
+  }
 };
-
+function slice<T>(arr: T[],offset:number,len:number): T[] {
+  const ret = arr.slice(offset,offset+len)
+  return ret
+}
 export function pieceTableExample() {
   const txt = "a".split('')
-  const pt = new PieceTable2(txt);
 
-  pt.insert("b".split(''), 1);
-  pt.insert("c".split(''), 2);
+  const pt = PieceTable([..."a"]);
 
-// Delete the previously inserted sentence
-  pt.delete(1, 1);
+  pt.insert([..."b"], 1);
+  pt.insert([..."c"], 2);
 
+  pt.remove(1, 1);
+  //pt.getHistories().map(log)
+  return
   var sequence = pt.getSequence();
-// sequence == "This is a document with some text."
 
   var subString = pt.stringAt(0, 1);
-// subString == "document"
 
 // PieceTable is an iterable:
   for (let character of pt) {
