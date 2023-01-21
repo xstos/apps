2dc34e7dc39095153dc5f3f806e61035f12e878c
Chris C <xstos@users.noreply.github.com>
2023-01-21 00:21:13 -0500

    so cool lol

--- playground/js/kreact-proxy/src/experiments/rx-dom.tsx
+++ playground/js/kreact-proxy/src/experiments/rx-dom.tsx
@@ -6,6 +6,7 @@ import {ipsum} from "../lorem"
 
 import Graph from 'graphology';
 import ReactDOM from "react-dom"
+import clonedeep from "lodash.clonedeep"
 
 declare var v: any
 declare var o: any
@@ -56,8 +57,8 @@ export function jsx(type: any, props: Record<string, any>, ...children: any[]) {
   return {type,props,children}
 }
 function ShimComponent(props) {
-  const { setValueCallback } = props
-  const [value,setValue] = useState(props.node)
+  const { setValueCallback, innerRef: ref, node } = props
+  const [value,setValue] = useState(node)
   setValueCallback(setValue)
   const children = value.children.map(child=>{
     if (has(child,'type')) {
@@ -65,26 +66,53 @@ function ShimComponent(props) {
     }
     return child
   })
-  return React.createElement(value.type,value.props,...children)
+
+  return React.createElement(value.type,{...value.props, ref},...children)
 }
 function processJsx(node, index) {
   let {type,props,children} = node
+  if (!has(props, 'style')) {
+    props.style={}
+  }
   if (!has(node, 'reactElement')) {
+    function update(callback) {
+      const cloned = clonedeep(node)
+      let newVal = callback(cloned)
+      if (newVal === undefined) {
+        newVal = cloned
+        node=cloned
+      }
+      node.setValue(newVal)
+    }
+    node.update=update
     function setValueCallback(callback) {
       node.setValue = callback
     }
-    node.reactElement = React.createElement(ShimComponent,{ setValueCallback, node })
+    function innerRef(el) {
+      node.el=el
+    }
+    node.reactElement = React.createElement(ShimComponent,{ setValueCallback, node, innerRef })
   }
   children.forEach((c,i)=>{
     if (has(c,'type')) {
       processJsx(c, i)
     }
   })
-
+  log('render',node)
   return node
 }
+const words = ipsum.split(' ')
+const wordEls = words.map((word,i)=>{
+  return <button id={i} word={word}>{i}</button>
+})
+function makeChild(props) {
+  const { left, top, value, index } = props
+  return <div id ={index} style={{position: 'absolute', left, top }}>{value}</div>
+}
+
 const container = <div id={'foo'} style={{overflow: 'clip', position: 'relative', width: '100%', height: '100%'}}>
-  <div id ={'someChild'} style={{position: 'absolute', backgroundColor: 'red', left: '5ch', top: '5ch', }}>someChild</div>
+  <div id ={'someChild'} style={{position: 'absolute', left: '5ch', top: '5ch', }}>someChild</div>
+  {wordEls}
 </div>
 
 processJsx(container, 0)
@@ -93,9 +121,11 @@ ReactDOM.render(container.reactElement, document.getElementById('root'))
 const childNode = container.children[0]
 childNode.setValue({...childNode, children: ['yo']})
 childNode.setValue({...childNode, children: ['yo2']})
+childNode.update((n)=>{
+  n.props.style.backgroundColor='red'
+})
 
 
-const words = ipsum.split(' ')
 const sheet = {
   globalOffset: cellx([0,0])
 }
@@ -146,7 +176,11 @@ window.addEventListener("resize", e=>{
 })
 
 document.addEventListener("pointermove",e=>{
-  //set('x',e.x)
+  childNode.update((n)=>{
+    n.props.style.left=e.x+'px'
+    n.props.style.top=e.y+'px'
+  })
+
 })
 
 export const rxDomExperiment = 2
