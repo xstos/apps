815c2f3925eefe44060a414c25e3a2d4d5b07b0e
Chris C <xstos@users.noreply.github.com>
2024-12-19 20:49:32 -0500

    wip

--- playground/js/VirtualizedGrid/main.js
+++ playground/js/VirtualizedGrid/main.js
@@ -3,9 +3,7 @@ function makeEl(html, append) {
     el.innerHTML = html
     el.style.visibility = "hidden"
     el.style.position = "relative"
-    //el.style.display="inline-block"
     append.appendChild(el)
-    el.style.backgroundColor = "yellow"
     return el;
 }
 // Usage
@@ -33,36 +31,78 @@ class Grid extends HTMLElement {
         s.overflow="scroll"
         var tileEl = makeEl('M', thisEl)
         tileEl.style.position="absolute"
+
         var heightEl = makeEl('', thisEl)
         heightEl.style.width="1px"
         heightEl.setAttribute('data-n',"vertscroll")
+
         var widthEl = makeEl('',thisEl)
         widthEl.setAttribute('data-n',"horizscroll")
         widthEl.style.height="1px"
-        widthEl.style.visibility='collapse'
 
-        var test = makeEl('A', thisEl)
-        test.style.backgroundColor=''
-        test.style.position = "sticky"
-        test.style.visibility=''
-        test.style.border="1px solid yellow"
-
+        var g = makeEl('', thisEl);
+        var gs = g.style;
+        gs.visibility=''
+        gs.display="grid"
+        gs.position = "sticky"
+        gs.top="0px"
+        gs.left="0px"
+        gs.border="1px solid yellow"
+        gs.overflow="hidden"
         var tileSize = {width:0, height:0}
         var ro = new ResizeObserver(sizeChanged)
         var numCols = 0
         var numRows = 0
+        var numCells = 0
+        var ourSize = {width:0,height:0}
         sizeChanged()
         function sizeChanged() {
             tileSize = tileEl.getBoundingClientRect();
-            var ourSize = thisEl.getBoundingClientRect()
+            var oldSize= ourSize
+            ourSize = thisEl.getBoundingClientRect()
+            if (ourSize.width===oldSize.width && ourSize.height===oldSize.height) {
+                return
+            }
+            log('sizeChanged')
+
+            //gs.display="none"
 
             var wrecip = 1.0/tileSize.width
             var hrecip = 1.0/tileSize.height
-            numCols = Math.floor(ourSize.width*wrecip)
-            numRows = Math.floor(ourSize.height*hrecip)
-            test.style.width=ourSize.width-scrollbarSize.width +"px"
-            test.style.height=ourSize.height-scrollbarSize.height +"px"
-            log(ourSize, tileSize, numRows, numCols)
+            let innerWidth = ourSize.width-scrollbarSize.width;
+            let innerHeight = ourSize.height-scrollbarSize.height;
+            gs.width=innerWidth +"px"
+            gs.height=innerHeight +"px"
+            numCols = Math.floor(innerWidth*wrecip)
+            numRows = Math.floor(innerHeight*hrecip)
+            numCells = numRows*numCols
+            gs.gridTemplateColumns=`repeat(${numCols},${tileSize.width}px)`
+            gs.gridTemplateRows=`repeat(${numRows},${tileSize.height}px)`
+            var numCellsToAdd = numCells-g.childNodes.length
+            var numCellsToRemove = g.childNodes.length-numCells
+            var df = document.createDocumentFragment()
+            for (let i = 0; i < numCellsToAdd; i++) {
+                let node = createElement('span');
+                node.innerHTML = 'a'
+                df.appendChild(node)
+            }
+            if (df.children.length>0) {
+                g.appendChild(df)
+            }
+            for (let i = 0; i < numCellsToRemove; i++) {
+                g.lastElementChild.remove()
+            }
+            var ix = 0
+            for (let i = 0; i < numCols; i++) {
+                for (let j = 0; j < numRows; j++) {
+                    var n= g.children[ix];
+                    n.id=`${i} ${j}`
+                    n.setAttribute('data-num',ix+'')
+                    ix++
+                }
+            }
+            gs.display='grid'
+            //log(ourSize, tileSize, numRows, numCols)
 
         }
 
@@ -74,12 +114,10 @@ class Grid extends HTMLElement {
             height(value) {
                 let h = value * tileSize.height;
                 heightEl.style.height = h+"px"
-                //test.style.top = -h+"px"
             },
             width(value) {
                 let w = value * tileSize.width;
                 widthEl.style.width = w+"px"
-                //test.style.left=-w+"px"
             }
         }
         function msg(key,value) {
@@ -115,5 +153,5 @@ customElements.define('x-grid',Grid)
 
 var grid = getEl("root")
 
-//grid.msg('height',100)
-//grid.msg('width',100)
+grid.msg('height',100)
+grid.msg('width',100)
