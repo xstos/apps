b5b61891ec586970d780d02814bfd136401a97b3
Chris C <xstos@users.noreply.github.com>
2026-02-13 09:48:18 -0500

    another avalonia experiment

---
+++ playground/csharp/derpide/derpide/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace derpide.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+}
---
+++ playground/csharp/derpide/derpide/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace derpide.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
