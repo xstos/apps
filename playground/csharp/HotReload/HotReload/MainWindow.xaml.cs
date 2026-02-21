2cd5fbe614447cdad74dd083ad177d3a297ff16e
Chris C <xstos@users.noreply.github.com>
2026-02-21 17:07:05 -0500

    editor supposed to hover on mouseover (doesnt work)
    Stupid simple HotReload in .net 4.72 WPF

---
+++ playground/csharp/HotReload/HotReload/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using Mono.CSharp;
+
+namespace HotReload
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Evaluator Mono;
+        public static MainWindow Win { get; set; }
+        static string LoadFile(string path)
+        {
+            try
+            {
+                using(FileStream fileStream = new FileStream(
+                          path,
+                          FileMode.Open,
+                          FileAccess.Read,
+                          FileShare.ReadWrite))
+                {
+                    using(StreamReader streamReader = new StreamReader(fileStream))
+                    {
+                        return streamReader.ReadToEnd();
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error loading log file: " + ex.Message);
+            }
+
+            return "";
+        } 
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += (sender, args) =>
+            {
+                Win = this;
+                Mono = new Mono.CSharp.Evaluator(new CompilerContext(new CompilerSettings(), new ConsoleReportPrinter()));
+                Mono.ReferenceAssembly(Assembly.GetExecutingAssembly());
+                void reload()
+                {
+                    try
+                    {
+                        var name = "X"+DateTime.Now.Ticks;
+                        var src = LoadFile(@"C:\repos\xstos\apps\playground\csharp\HotReload\HotReload\src\Hot.cs");
+                        src = src.Replace("RENAME_ME", name);
+                        Mono.Run(src);
+                        Mono.Run($"{name}.Hot.Run();");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                    
+                }
+                reload();
+                var fsw = new FileSystemWatcher(@"C:\repos\xstos\apps\playground\csharp\HotReload\HotReload\src\");
+                fsw.EnableRaisingEvents = true;
+                
+                fsw.Changed += (o, eventArgs) =>
+                {
+                    var path = eventArgs.FullPath.ToLower();
+                    if (path.EndsWith("~")) return;
+                    if (!path.EndsWith("hot.cs"))
+                    {
+                        var src = LoadFile(path);
+                        try
+                        {
+                            Mono.Run(src);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                    }
+
+                    reload();
+                };
+            };
+        }
+    }
+}
---
+++ playground/csharp/HotReload/HotReload/src/Hot.cs
@@ -0,0 +1,19 @@
+﻿
+namespace RENAME_ME
+{
+    public static class Hot
+    {
+        public static void Run()
+        {
+            HotReload.MainWindow.Win.Dispatcher.Invoke(() =>
+            {
+                HotReload.MainWindow.Win.Title = ""._Get();
+
+            });
+        }
+        public static string _Get(this object o)
+        {
+            return "D";
+        }
+    }
+}
