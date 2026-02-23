a236b41456ed068567f65cd3d518d286a95e2910
Chris C <xstos@users.noreply.github.com>
2026-02-23 05:30:17 -0500

    unhardcode hot path

--- playground/csharp/HotReload/HotReload/MainWindow.xaml.cs
+++ playground/csharp/HotReload/HotReload/MainWindow.xaml.cs
@@ -15,15 +15,12 @@ namespace HotReload
     {
         public static Evaluator Mono;
         public static MainWindow Win { get; set; }
+        static int Count=0;
         static string LoadFile(string path)
         {
             try
             {
-                using(FileStream fileStream = new FileStream(
-                          path,
-                          FileMode.Open,
-                          FileAccess.Read,
-                          FileShare.ReadWrite))
+                using(FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                 {
                     using(StreamReader streamReader = new StreamReader(fileStream))
                     {
@@ -38,24 +35,33 @@ namespace HotReload
             }
 
             return "";
-        } 
+        }
+
+        static string GetSrcPath()
+        {
+            var location = Assembly.GetEntryAssembly().Location;
+            var fi = new FileInfo(location);
+            var dir = fi.DirectoryName+"../../../../src";
+            return Path.GetFullPath(dir);
+        }
         public MainWindow()
         {
             InitializeComponent();
             this.Loaded += (sender, args) =>
             {
                 Win = this;
-                Mono = new Mono.CSharp.Evaluator(new CompilerContext(new CompilerSettings(), new ConsoleReportPrinter()));
+                Mono = new Evaluator(new CompilerContext(new CompilerSettings(), new ConsoleReportPrinter()));
                 Mono.ReferenceAssembly(Assembly.GetExecutingAssembly());
                 Mono.ReferenceAssembly(typeof(DockPanel).Assembly);
                 Mono.ReferenceAssembly(typeof(SolidColorBrush).Assembly);
-
+                var srcPath = GetSrcPath();
+                var hotPath = Path.Combine(srcPath, "Hot.cs");
                 void reload()
                 {
                     try
                     {
-                        var name = "X"+DateTime.Now.Ticks;
-                        var src = LoadFile(@"C:\repos\xstos\apps\playground\csharp\HotReload\HotReload\src\Hot.cs");
+                        var name = "X"+Count++;
+                        var src = LoadFile(hotPath);
                         src = src.Replace("RENAME_ME", name);
                         Mono.Run(src);
                         Mono.Run($"{name}.Hot.Run();");
@@ -67,7 +73,7 @@ namespace HotReload
                     
                 }
                 reload();
-                var fsw = new FileSystemWatcher(@"C:\repos\xstos\apps\playground\csharp\HotReload\HotReload\src\");
+                var fsw = new FileSystemWatcher(srcPath);
                 fsw.EnableRaisingEvents = true;
                 
                 fsw.Changed += (o, eventArgs) =>
--- playground/csharp/HotReload/HotReload/src/Hot.cs
+++ playground/csharp/HotReload/HotReload/src/Hot.cs
@@ -1,29 +1,45 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
+
 namespace RENAME_ME
 {
-
-
     public static class Hot
     {
         public static void Run()
         {
             var win = HotReload.MainWindow.Win;
-            HotReload.MainWindow.Mono.ReferenceAssembly(typeof(DockPanel).Assembly);
+
 
             //
             win.Dispatcher.Invoke(() =>
             {
-                win.Title = ""._Get();
+                win.Title = "hello world";
                 var dock = new DockPanel();
                 win.Content = dock;
-                dock.Background = new SolidColorBrush(Colors.Black);
-                
+
+                dock.Children.Add(new Button() { Content = "hi" }.SetDock(Dock.Top));
+                var box = new TextBox();
+                dock.Children.Add(box);
+                Action<string> Write = (t) =>
+                {
+                    box.Text += t;
+                };
 ;            });
         }
-        public static string _Get(this object o)
+        
+        public static T SetDock<T>(this T c, Dock d) where T: UIElement
         {
-            return "derp"; //
+            DockPanel.SetDock(c,d);
+            return c;
+        }
+
+        public static string Up(this string path)
+        {
+            return null;
         }
     }
 }
