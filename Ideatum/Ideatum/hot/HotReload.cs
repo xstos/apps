31d0f1358bbdaaf713c5635da5aab9e3fb482932
Chris C <xstos@users.noreply.github.com>
2026-03-12 07:07:58 -0400

    wip

--- Ideatum/Ideatum/hot/HotReload.cs
+++ Ideatum/Ideatum/hot/HotReload.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
@@ -24,16 +26,17 @@ namespace RENAME_ME
             int letterHeight = 512;
             var NextColor = Program.MakeGetNextHue(1000);
             var ints = GetLetterPixels();
+            
             int[] GetLetterPixels()
             {
                 var canvas = new Canvas { Width = letterWidth, Height = letterHeight };
-                canvas.Background = new SolidColorBrush(Colors.Green);
+                canvas.Background = new SolidColorBrush(Colors.LimeGreen);
                 canvas.Children.Add(new TextBlock
                 {
                     Text = "道",
-                    FontSize = 180,
-                    Background = new SolidColorBrush(Colors.Red),
-                    Foreground = Brushes.Cyan,
+                    FontSize = 420,
+                    Background = new SolidColorBrush(Colors.Blue),
+                    Foreground = Brushes.White,
                     FontFamily = new FontFamily("Courier New")
                 });
 
@@ -74,15 +77,22 @@ namespace RENAME_ME
             // Project 3D to 2D
             PointF[] points = new PointF[4];
             float fov = 256; // Simple perspective factor
-            float zoffs = -1f;
-            float xoffs = 0.7f;
-            float yoffs = 0.4f;
-            void Render(int[] surface, int width, int height)
+            float zoffs = 6f;
+            float xoffs = 0f;
+            float yoffs = -5f;
+            Action<int[], int, int> renderAction = Clear;
+            void Clear(int[] surface, int width, int height)
             {
                 for (int i = 0; i < surface.Length; i++) //clear black
                 {
-                    //surface[i] = 0;
+                    surface[i] = 0;
                 }
+
+                renderAction = Render;
+            }
+            void Render(int[] surface, int width, int height)
+            {
+                
                 for (int i = 0; i < 4; i++)
                 {
                     float x = verts[i, 0]+xoffs;
@@ -91,7 +101,7 @@ namespace RENAME_ME
 
                     // Simple perspective projection
                     points[i] = new PointF(
-                        400 + x * fov / z,
+                        100 + x * fov / z,
                         800 - y * fov / z
                     );
                 }
@@ -101,7 +111,6 @@ namespace RENAME_ME
                 // Draw the textured square using triangles
                 DrawTexturedTriangle(canvasSprite, texSprite, points[0], points[2], points[3], bl, tl, tr); //top left half
                 DrawTexturedTriangle(canvasSprite, texSprite, points[0], points[1], points[2], bl, tr, br); //bottom left half
-            
             }
 
             void Memory2DExample(int[] surface, int width, int height)
@@ -125,7 +134,7 @@ namespace RENAME_ME
                     }
                 }
             }
-
+            
             void DoRender()
             {
                 var surface = Program.Surface;
@@ -134,12 +143,13 @@ namespace RENAME_ME
                 var len = width * height;
                 var numPixels = surface.Length;
                 if (len != numPixels) return; //race condition caught between Width/Height update;
-                Render(surface, width, height);
+                renderAction(surface, width, height);
             }
 
             Program.Render = DoRender;
         }
-
+        
+        // Simple software rasterization
         static void DrawTexturedTriangle(Sprite canvas, Sprite tex, PointF p1, PointF p2, PointF p3,
             (float, float) uv1, (float, float) uv2, (float, float) uv3)
         {
@@ -160,42 +170,54 @@ namespace RENAME_ME
             var dp2p1X = p2.X - p1.X;
             var dp2p1Y = p2.Y - p1.Y;
             var dp3p1Y = p3.Y - p1.Y;
-            // Simple software rasterization
 
-            //var BD = ctl.RazorBMP.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             var canvasSurface = canvas.Surface;
             var canvasStride = width;
 
-            //var BD2 = tex.LockBits(new Rectangle(0, 0, tex.Width, tex.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             var texSurface = tex.Surface;
             var texStride = tex.Width;
             int offs;
             int offs2;
+            var w0denom = dp2p1X * dp3p1Y - dp2p1Y * dp3p1X;
+            var w1denom = dp3p1X * dp2p1Y - dp3p1Y * dp2p1X;
+            w0denom = 1.0f / w0denom;
+            w1denom = 1.0f / w1denom;
+            var (uv1X, uv1Y) = uv1;
+            var (uv2X, uv2Y) = uv2;
+            var (uv3X, uv3Y) = uv3;
+            var texWidthSub1 = texWidth - 1;
+            var texHeightSub1 = texHeight - 1;
+            float w0;
+            float w1;
+            float w2;
+            float u;
+            float v;
+            int tx;
+            int ty;
+            float dyp1Y;
+            float dxp1X;
             for (int y = minY; y <= maxY; y++)
             for (int x = minX; x <= maxX; x++)
             {
                 // Barycentric coordinates
-                var dyp1Y = y - p1.Y;
-                var dxp1X = x - p1.X;
-                float w0 = (dp2p1X * dyp1Y - dp2p1Y * dxp1X) / (dp2p1X * dp3p1Y - dp2p1Y * dp3p1X);
-                float w1 = (dp3p1X * dyp1Y - dp3p1Y * dxp1X) / (dp3p1X * dp2p1Y - dp3p1Y * dp2p1X);
-                float w2 = 1 - w0 - w1;
+                dyp1Y = y - p1.Y;
+                dxp1X = x - p1.X;
+                w0 = (dp2p1X * dyp1Y - dp2p1Y * dxp1X) * w0denom;
+                w1 = (dp3p1X * dyp1Y - dp3p1Y * dxp1X) * w1denom;
+                w2 = 1 - w0 - w1;
 
                 // Check if point is inside triangle
-
-                if (w0 >= 0 && w1 >= 0 && w2 >= 0)
-                {
-                    // Interpolate texture coordinates
-                    float u = uv1.Item1 * w2 + uv2.Item1 * w0 + uv3.Item1 * w1;
-                    float v = uv1.Item2 * w2 + uv2.Item2 * w0 + uv3.Item2 * w1;
-                    // Sample texture
-                    int tx = (int)(u * (texWidth - 1));
-                    int ty = (int)(v * (texHeight - 1));
-                    offs2 = ty * texStride + tx;
-                    //Color c = tex[tx, ty];
-                    offs = y * canvasStride + x;
-                    canvasSurface[offs] = texSurface[offs2];
-                }
+                if (!(w0 >= 0) || !(w1 >= 0) || !(w2 >= 0)) continue;
+                // Interpolate texture coordinates
+                u = uv1X * w2 + uv2X * w0 + uv3X * w1;
+                v = uv1Y * w2 + uv2Y * w0 + uv3Y * w1;
+                // Sample texture
+                tx = (int)(u * texWidthSub1);
+                ty = (int)(v * texHeightSub1);
+                offs2 = ty * texStride + tx;
+                //Color c = tex[tx, ty];
+                offs = y * canvasStride + x;
+                canvasSurface[offs] = texSurface[offs2];
             }
         }
     }
@@ -205,15 +227,12 @@ namespace RENAME_ME
         public int[] Surface;
         public int Width;
         public int Height;
-        public int Stride;
-        public static int BitsPerPixel = Marshal.SizeOf<int>();
 
         public Sprite(int[] surface, int width, int height)
         {
             Surface = surface;
             Width = width;
             Height = height;
-            Stride = (width * BitsPerPixel + 7) / 8;
         }
     }
 }
